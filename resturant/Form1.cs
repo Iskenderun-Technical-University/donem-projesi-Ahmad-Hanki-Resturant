350fb4767b3eda4883165aa7efb85acf33e5c3ff
ahmad <itxti909@gmail.com>
2023-03-30 18:23:02 +0300

    Fixed Issue

--- resturant/Form1.cs
+++ resturant/Form1.cs
@@ -32,7 +32,7 @@ namespace resturant
         {
 
         }
-
+        Form2 form2 = new Form2();
         private void Home_Click(object sender, EventArgs e)
         {
             panelSepete.Visible = false;
@@ -63,7 +63,7 @@ namespace resturant
 
         private void orders_Click(object sender, EventArgs e)
         {
-
+            form2.ShowDialog();
         }
 
         private void panel2_Paint(object sender, PaintEventArgs e)
@@ -82,7 +82,8 @@ namespace resturant
             n1.Text = x1.ToString();
              i1 = 55 * x1;
             p1.Text = i1.ToString()+"TL";
-            
+            int z = top();
+            Toplam.Text = z.ToString() + "TL";
         }
 
         private void c1_Click(object sender, EventArgs e)
@@ -94,7 +95,8 @@ namespace resturant
              i1 = x1 * 55;
             n1.Text = x1.ToString();
             p1.Text = i1.ToString() + "TL";
-            
+            int z = top();
+            Toplam.Text = z.ToString() + "TL";
         }
         
         private void bunifuButton1_Click(object sender, EventArgs e)
@@ -103,7 +105,8 @@ namespace resturant
             n1.Text = x1.ToString();
             i1= 55 * x1;
             p1.Text = i1.ToString()+"TL";
-
+            int z = top();
+            Toplam.Text = z.ToString() + "TL";
         }
 
         private void n1_Click(object sender, EventArgs e)
@@ -115,11 +118,22 @@ namespace resturant
         {
 
         }
-
         private void bunifuButton24_Click(object sender, EventArgs e)
         {
-            int toplam = i1 + i2+i3;
-            Toplam.Text = toplam.ToString()+"TL";
+            int z = top();
+            if (z==0)
+            {
+                MessageBox.Show("Bir şey eklemeniz gerekiyor","Eklenmemiş",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                return;
+            }
+            else
+            {
+                DialogResult result = MessageBox.Show("Onyalamak ister misiniz?", "Onaylamak", MessageBoxButtons.OKCancel);
+                if (result == DialogResult.OK)
+                {
+                    form2.ShowDialog();
+                }
+            }
         }
         int x2;
         int i2;
@@ -130,6 +144,8 @@ namespace resturant
             n5.Text = x5.ToString();
             i5 = 59 * x5;
             p5.Text = i5.ToString() + "TL";
+            int z = top();
+            Toplam.Text = z.ToString() + "TL";
         }
 
         private void e6_Click(object sender, EventArgs e)
@@ -138,6 +154,8 @@ namespace resturant
             n6.Text = x6.ToString();
             i6 = 29 * x6;
             p6.Text = i6.ToString() + "TL";
+            int z = top();
+            Toplam.Text = z.ToString() + "TL";
         }
 
         private void e7_Click(object sender, EventArgs e)
@@ -146,6 +164,8 @@ namespace resturant
             n7.Text = x7.ToString();
             i7 = 25 * x7;
             p7.Text = i7.ToString() + "TL";
+            int z = top();
+            Toplam.Text = z.ToString() + "TL";
         }
 
         private void se6_Click(object sender, EventArgs e)
@@ -154,6 +174,8 @@ namespace resturant
             n6.Text = x6.ToString();
             i6 = 29 * x6;
             p6.Text = i6.ToString() + "TL";
+            int z = top();
+            Toplam.Text = z.ToString() + "TL";
         }
 
         private void se7_Click(object sender, EventArgs e)
@@ -162,6 +184,8 @@ namespace resturant
             n7.Text = x7.ToString();
             i7 = 25 * x7;
             p7.Text = i7.ToString() + "TL";
+            int z = top();
+            Toplam.Text = z.ToString() + "TL";
         }
 
         private void se8_Click(object sender, EventArgs e)
@@ -170,6 +194,8 @@ namespace resturant
             n8.Text = x8.ToString();
             i8 = 20 * x8;
             p8.Text = i8.ToString() + "TL";
+            int z = top();
+            Toplam.Text = z.ToString() + "TL";
         }
 
         private void se9_Click(object sender, EventArgs e)
@@ -178,6 +204,8 @@ namespace resturant
             n9.Text = x9.ToString();
             i9 = 15 * x9;
             p9.Text = i9.ToString() + "TL";
+            int z = top();
+            Toplam.Text = z.ToString() + "TL";
         }
 
         private void Sandwitch_Click(object sender, EventArgs e)
@@ -210,6 +238,8 @@ namespace resturant
             n10.Text = x10.ToString();
             i10 = 3 * x10;
             p10.Text = i10.ToString() + "TL";
+            int z = top();
+            Toplam.Text = z.ToString() + "TL";
         }
 
         private void se11_Click(object sender, EventArgs e)
@@ -218,6 +248,8 @@ namespace resturant
             n11.Text = x11.ToString();
             i11 = 13 * x11;
             p11.Text = i11.ToString() + "TL";
+            int z = top();
+            Toplam.Text = z.ToString() + "TL";
         }
 
         private void se12_Click(object sender, EventArgs e)
@@ -226,6 +258,8 @@ namespace resturant
             n12.Text = x12.ToString();
             i12 = 5 * x12;
             p12.Text = i12.ToString() + "TL";
+            int z = top();
+            Toplam.Text = z.ToString() + "TL";
         }
 
         private void e8_Click(object sender, EventArgs e)
@@ -234,6 +268,8 @@ namespace resturant
             n8.Text = x8.ToString();
             i8 = 20 * x8;
             p8.Text = i8.ToString() + "TL";
+            int z = top();
+            Toplam.Text = z.ToString() + "TL";
         }
 
         private void e9_Click(object sender, EventArgs e)
@@ -242,6 +278,8 @@ namespace resturant
             n9.Text = x9.ToString();
             i9 = 15 * x9;
             p9.Text = i9.ToString() + "TL";
+            int z = top();
+            Toplam.Text = z.ToString() + "TL";
         }
 
         private void e10_Click(object sender, EventArgs e)
@@ -250,6 +288,8 @@ namespace resturant
             n10.Text = x10.ToString();
             i10 = 3 * x10;
             p10.Text = i10.ToString() + "TL";
+            int z = top();
+            Toplam.Text = z.ToString() + "TL";
         }
 
         private void e11_Click(object sender, EventArgs e)
@@ -258,6 +298,8 @@ namespace resturant
             n11.Text = x11.ToString();
             i11 = 13 * x11;
             p11.Text = i11.ToString() + "TL";
+            int z = top();
+            Toplam.Text = z.ToString() + "TL";
         }
 
         private void e12_Click(object sender, EventArgs e)
@@ -266,6 +308,8 @@ namespace resturant
             n12.Text = x12.ToString();
             i12 = 5 * x12;
             p12.Text = i12.ToString() + "TL";
+            int z = top();
+            Toplam.Text = z.ToString() + "TL";
         }
 
         private void c4_Click(object sender, EventArgs e)
@@ -277,6 +321,8 @@ namespace resturant
             i4 = x4 * 30;
             n4.Text = x4.ToString();
             p4.Text = i4.ToString() + "TL";
+            int z = top();
+            Toplam.Text = z.ToString() + "TL";
         }
 
         private void c5_Click(object sender, EventArgs e)
@@ -288,6 +334,8 @@ namespace resturant
             i5 = x5 * 59;
             n5.Text = x5.ToString();
             p5.Text = i5.ToString() + "TL";
+            int z = top();
+            Toplam.Text = z.ToString() + "TL";
         }
 
         private void c6_Click(object sender, EventArgs e)
@@ -299,6 +347,86 @@ namespace resturant
             i6 = x6 * 25;
             n6.Text = x6.ToString();
             p6.Text = i6.ToString() + "TL";
+            int z = top();
+            Toplam.Text = z.ToString() + "TL";
+        }
+
+        private void c9_Click(object sender, EventArgs e)
+        {
+            if (x9 >= 1)
+            {
+                x9--;
+            }
+            i9 = x9 * 15;
+            n9.Text = x9.ToString();
+            p9.Text = i9.ToString() + "TL";
+            int z = top();
+            Toplam.Text = z.ToString() + "TL";
+        }
+
+        private void c8_Click(object sender, EventArgs e)
+        {
+            if (x8 >= 1)
+            {
+                x8--;
+            }
+            i8 = x8 * 20;
+            n8.Text = x8.ToString();
+            p8.Text = i8.ToString() + "TL";
+            int z = top();
+            Toplam.Text = z.ToString() + "TL";
+        }
+
+        private void c7_Click(object sender, EventArgs e)
+        {
+            if (x7 >= 1)
+            {
+                x7--;
+            }
+            i7 = x7 * 25;
+            n7.Text = x7.ToString();
+            p7.Text = i7.ToString() + "TL";
+            int z = top();
+            Toplam.Text = z.ToString() + "TL";
+        }
+
+        private void c10_Click(object sender, EventArgs e)
+        {
+            if (x10 >= 1)
+            {
+                x10--;
+            }
+            i10 = x10 * 3;
+            n10.Text = x10.ToString();
+            p10.Text = i10.ToString() + "TL";
+            int z = top();
+            Toplam.Text = z.ToString() + "TL";
+        }
+
+        private void c11_Click(object sender, EventArgs e)
+        {
+            if (x11 >= 1)
+            {
+                x11--;
+            }
+            i11 = x11 * 13;
+            n11.Text = x11.ToString();
+            p11.Text = i11.ToString() + "TL";
+            int z = top();
+            Toplam.Text = z.ToString() + "TL";
+        }
+        
+        private void c12_Click(object sender, EventArgs e)
+        {
+            if (x12 >= 1)
+            {
+                x12--;
+            }
+            i12 = x12 * 5;
+            n12.Text = x12.ToString();
+            p12.Text = i12.ToString() + "TL";
+            int z = top();
+            Toplam.Text = z.ToString() + "TL";
         }
 
         private void se2_Click(object sender, EventArgs e)
@@ -307,7 +435,8 @@ namespace resturant
             n2.Text = x2.ToString();
             i2 = 99 * x2;
             p2.Text = i2.ToString() + "TL";
-
+            int z = top();
+            Toplam.Text = z.ToString() + "TL";
         }
 
         private void e2_Click(object sender, EventArgs e)
@@ -316,6 +445,8 @@ namespace resturant
             n2.Text = x2.ToString();
             i2 = 99 * x2;
             p2.Text = i2.ToString() + "TL";
+            int z = top();
+            Toplam.Text = z.ToString() + "TL";
         }
 
         private void c2_Click(object sender, EventArgs e)
@@ -327,6 +458,8 @@ namespace resturant
             i2 = x2 * 55;
             n2.Text = x2.ToString();
             p2.Text = i2.ToString() + "TL";
+            int z = top();
+            Toplam.Text = z.ToString() + "TL";
         }
         int x3;
         int i3;
@@ -336,6 +469,8 @@ namespace resturant
             n3.Text = x3.ToString();
             i3 = 45 * x3;
             p3.Text = i3.ToString() + "TL";
+            int z = top();
+            Toplam.Text = z.ToString() + "TL";
         }
 
         private void e3_Click(object sender, EventArgs e)
@@ -344,6 +479,8 @@ namespace resturant
             n3.Text = x3.ToString();
             i3 = 45 * x3;
             p3.Text = i3.ToString() + "TL";
+            int z = top();
+            Toplam.Text = z.ToString() + "TL";
         }
 
         private void c3_Click(object sender, EventArgs e)
@@ -355,6 +492,8 @@ namespace resturant
             i3 = x3 * 55;
             n3.Text = x3.ToString();
             p3.Text = i3.ToString() + "TL";
+            int z = top();
+            Toplam.Text = z.ToString() + "TL";
         }
 
         private void se5_Click(object sender, EventArgs e)
@@ -363,6 +502,8 @@ namespace resturant
             n5.Text = x5.ToString();
             i5 = 59 * x5;
             p5.Text = i5.ToString() + "TL";
+            int z = top();
+            Toplam.Text = z.ToString() + "TL";
         }
         int x4;
         int i4;
@@ -372,11 +513,8 @@ namespace resturant
             n4.Text = x4.ToString();
             i4 = 30 * x4;
             p4.Text = i4.ToString() + "TL";
-        }
-
-        private void label15_Click(object sender, EventArgs e)
-        {
-
+            int z = top();
+            Toplam.Text = z.ToString() + "TL";
         }
 
         private void bunifuButton7_Click(object sender, EventArgs e)
@@ -385,6 +523,22 @@ namespace resturant
             n4.Text = x4.ToString();
             i4 = 30 * x4;
             p4.Text = i4.ToString() + "TL";
+            int z = top();
+            Toplam.Text = z.ToString() + "TL";
+        }
+        public int top ()
+        {
+            int TOTAL = i1 + i2 + i3 + i4 + i5 + i6 + i7 + i8 + i9 + i10 + i11 + i12;
+            if (TOTAL>0)
+            {
+                Warning.Text = "Eklenmiş";
+                Warning.ForeColor = Color.Red;
+            }
+            if (TOTAL==0)
+            {
+                Warning.Text = "";
+            }
+            return TOTAL;
         }
     }
 }
